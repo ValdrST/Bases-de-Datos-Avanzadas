1b059f92bdd1fec2c53cae347c156dcdeb9e1848
Vicente Romero <valdr@pc-vra.fi.unam>
2021-03-02 21:09:13 -0600

    ejercicio 2

--- tema01/ejercicio-practico-02/s-01-procesa-imagenes.sh
+++ tema01/ejercicio-practico-02/s-01-procesa-imagenes.sh
@@ -1,47 +1,73 @@
+#!/bin/bash
 # @Autor Vicente Romero Andrade
 # @Fecha 22/02/2021
 # @Descripcion Procesa URL de imagenes
 
+ARCHIVO_IMAGENES=${1}
+NUM_IMAGENES=${2}
+ARCHIVO_ZIP=${3}
+
 function ayuda() {
-    echo "Para ejecutar el programa utilizar la siguiente sintaxis:
-    ./s-01-procesa-imagenes.sh <path_archivo_imagen> <num_imagenes> [<nombre_archivo_zip>]
-    <path_archivo_imagen> Ruta y nombre donde se encuentra el archivo de texto que contiene un URL
-    por cada renglón de la imagen que será descargada de internet.
-    <num_imagenes> Total de imágenes a descargar, entre 1 y hasta 90 imágenes
-    [<nombre_archivo_zip>] Parámetro opcional que indica la ruta y nombre del archivo zip a generar. 
-    En caso de no especificarse se empleará el nombre 
-    imagenes-yyyy-mm-dd-hh:mi:ss.zip y estará ubicado en 
-    ${TMP}/imagenes/${USER}/imagenes. Si se especifica este parámetro, su valor 
-    deberá incluir la ruta y el nombre del archivo zip a generar. La ruta 
-    absoluta y el nombre del archivo zip generado deberá ser exportado empleando 
-    una variable de entorno IMG_ZIP_FILE"
+  STATUS="${1}"
+  cat s-02-ayuda.sh
+  exit "${STATUS}"
 }
 
-RUTA=${1}
-NUM_IMAGENES=${2}
-ZIP_GEN=${3}
-RE='^[0-9]+$'
-if ! [ "$RUTA" != '' ]; then
-  exit 100
-elif ! [ -f "$RUTA" ]; then
-  exit 101
-elif [[ "$NUM_IMAGENES" -le 1 || "$NUM_IMAGENES" -ge 90 ]] ; then
-  echo "102"
-  exit 102
-elif ! [ -f "$ZIP_GEN" ]; then
-    date
-elif ! [[ -f "$ZIP_GEN"  && "$ZIP_GEN" != "" ]]; then
-  echo "103"
-  exit 103
-else 
-  for((i=1;i<=NUM_IMAGENES;i++)); do
-      URL=$(sed "$i!d" "$RUTA")
-       wget -qo- "$URL" | gzip "$ZIP_GEN"
-      #RES=$(wget -qo- "$URL")
-      #echo "|$RES|"
-
-  done
-  
+if [ -z "${ARCHIVO_IMAGENES}" ]; then
+  echo "ERROR: El nombre del archivo de imágenes es obligatorio"
+  ayuda 100
+elif ! [ -f "${ARCHIVO_IMAGENES}" ]; then
+  echo "ERROR: El archivo ${ARCHIVO_IMAGENES} no existe"
+  ayuda 101
+elif ! [[ "${NUM_IMAGENES}" =~  [0-9]+ && "${NUM_IMAGENES}" -gt 0 && 
+  "${NUM_IMAGENES}" -le 90 ]] ; then
+  echo "ERROR: El parametro de numero de imagenes no es correcto"
+  ayuda 102
+elif [ -n "${ARCHIVO_ZIP}" ]; then
+  # Extraer la ruta del archivo zip
+  DIR_SALIDA=$(dirname "${ARCHIVO_ZIP}")
+  mkdir -p "${DIR_SALIDA}"
+  NOMBRE_ZIP=$(basename "${ARCHIVO_ZIP}")
+  if ! [ -d "${DIR_SALIDA}" ]; then
+    echo "ERROR: El directorio de salida no existe"
+    ayuda 103
+  fi
+else
+  DIR_SALIDA="/tmp/${USER}/imagenes"
+  mkdir -p "${DIR_SALIDA}"
+  NOMBRE_ZIP="imagenes-$(date '+%Y-%m-%H-%M-%S').zip"
 fi
 
+echo "Parámetros correctos, obteniendo imágenes..."
+# Limpiar directorio en caso de que no este vacio
+rm -f "${DIR_SALIDA}"/"${NOMBRE_ZIP}"
 
+# Leer el contenido del archivo que contiene la lista imágenes
+# que se van a descargas
+COUNT=1
+while read -r LINEA
+do
+  wget -q -P "${DIR_SALIDA}" "${LINEA}"
+  STATUS=$?
+  if ! [ "${STATUS}" -eq 0 ]; then
+    echo "ERROR: al descargar una imagen"
+    ayuda 104
+  fi
+  if [ "${COUNT}" -ge "${NUM_IMAGENES}" ]; then
+    break
+  fi
+  COUNT=$((COUNT+1))
+done < "${ARCHIVO_IMAGENES}"
+
+#Generando archivo zip
+export IMG_ZIP_FILE="${DIR_SALIDA}"/"${NOMBRE_ZIP}"
+zip -j "${IMG_ZIP_FILE}" "${DIR_SALIDA}"/*.jpg
+
+# Eliminar imágenes descargadas
+rm -rf "${DIR_SALIDA}"/*.jpg
+
+#Cambiar permisos
+chmod 700 "${DIR_SALIDA}"/"${NOMBRE_ZIP}"
+
+# Generar lista de archivos
+unzip -Z1 "${IMG_ZIP_FILE}" > "${DIR_SALIDA}"/s-00-lista-archivos.txt
