6c42d0d9d86ce496cc3b2cee9a896eab992b4b00
Valdr Stiglitz <dark_reggae_93@comunidad.unam.mx>
2021-03-13 23:08:27 -0500

    Ejercicio 1 tema 2

--- tema02/ejercicio-practico-01/s-01-database-info.sql
+++ tema02/ejercicio-practico-01/s-01-database-info.sql
@@ -1,8 +1,10 @@
 --@Autor: Vicente Romero Andrade
---@Fecha creación:  06/03/2021
---@Descripción:  Script 1 del ejercicio 1 tema 2.
+--@Fecha creacion:  06/03/2021
+--@Descripcion: Muestra info de la base
 CONNECT sys/system1 as sysdba;
-CREATE USER vra0201 IDENTIFIED BY VRA;
+CREATE USER vra0201 IDENTIFIED BY vra;
+GRANT CREATE TABLE, connect TO vra0201;
+ALTER USER vra0201 quota 100M on users;
 
 create table vra0201.database_info(
   instance_name varchar2(16),
@@ -35,28 +37,72 @@ INSERT INTO vra0201.database_info(
   max_component_mb
 ) VALUES
   (
+    --instance_name
     (SELECT INSTANCE_NAME 
       FROM v$instance),
+    --db_domain
     (SELECT value 
       FROM v$parameter 
       WHERE name='db_domain'),
+    --db_charset 
     (SELECT value 
       FROM nls_database_parameters 
       WHERE PARAMETER='NLS_CHARACTERSET'),
+    --sys_timestamp 
     (SELECT SYSTIMESTAMP 
       FROM DUAL),
-    (SELECT tz_offset((SELECT sessiontimezone FROM dual)) 
+    --timezome_offset 
+    (SELECT tz_offset((SELECT sessiontimezone FROM dual))
       FROM dual),
+    --db_block_size_bytes 
     (SELECT VALUE 
-      FROM v$parameter 
+    FROM v$parameter 
       WHERE name = 'db_block_size'),
-    (4096),
+    --os_block_size_bytes 
+    ('4096'),
+    --redo_block_size_bytes
     (SELECT BLOCKSIZE 
-      FROM v$log 
+    FROM v$log 
       WHERE GROUP#=1),
+    --total_components
     (SELECT count(*) total_components
-      FROM v$sysaux_occupants),
-    (SELECT round(sum(SPACE_USAGE_KBYTES),2) total_components_mb 
-      FROM v$sysaux_occupants),
-    ()
-  )
+    FROM v$sysaux_occupants),
+    --total_components_mb
+    (SELECT 
+      round(sum(SPACE_USAGE_KBYTES)/1024,2)
+    FROM v$sysaux_occupants),
+    --max_component_name
+    (SELECT OCCUPANT_NAME 
+    FROM (SELECT * 
+        FROM v$sysaux_occupants 
+        ORDER BY SPACE_USAGE_KBYTES DESC) 
+        WHERE ROWNUM = 1),
+    --max_component_desc 
+    (SELECT OCCUPANT_DESC 
+      FROM (SELECT * 
+        FROM v$sysaux_occupants 
+        ORDER BY SPACE_USAGE_KBYTES DESC) 
+        WHERE ROWNUM = 1),
+    --max_component_mb
+    (SELECT 
+      round(SPACE_USAGE_KBYTES / 1024,2)
+    FROM (SELECT * 
+      FROM v$sysaux_occupants 
+      ORDER BY SPACE_USAGE_KBYTES DESC) 
+      WHERE ROWNUM = 1)
+  );
+
+Prompt mostrando datos parte 1;
+set linesize window
+SELECT instance_name,db_domain,db_charset,sys_timestamp,timezome_offset 
+FROM vra0201.database_info;
+
+Prompt mostrando datos parte 2;
+SELECT db_block_size_bytes,os_block_size_bytes,redo_block_size_bytes,
+ total_components,total_components_mb
+FROM vra0201.database_info;
+
+Prompt mostrando datos parte 3;
+SELECT max_component_name,max_component_desc,max_component_mb
+FROM vra0201.database_info;
+
