61dd0c9aa7219c38dd5874603c498151cd09f394
Valdr Stiglitz <dark_reggae_93@comunidad.unam.mx>
2021-07-20 17:35:47 -0500

    mejoras

--- tema06/ejercicio-practico-02/s-01-crear-tabla.sql
+++ tema06/ejercicio-practico-02/s-01-crear-tabla.sql
@@ -3,12 +3,12 @@
 --@Descripción:  Script 2 del ejercicio 2 tema 6
 whenever sqlerror exit rollback
 set serveroutput on
-connect sys@vrabda2_dedicated/system2 as sysdba
+connect VRA0602/VRA0602
 -- B
 declare
   v_count number;
   v_username varchar2(30) := 'VRA0602';
-  v_table varchar2(30) := 'empleado';
+  v_table varchar2(30) := 'EMPLEADO';
 begin
   --Verificar si la table existe
   select count(*) into v_count
@@ -17,20 +17,40 @@ begin
   and owner = v_username;
   --Si existe la tabla, entonces se borra
   if v_count > 0 then
-    execute immediate 'drop table '|| v_username ||'.'||v_table;
+    execute immediate 'drop table '||v_table;
   end if;
-  execute immediate 'create table '|| v_username ||'.'||v_table||'(
+  execute immediate 'create table '||v_table||' (
     id number,
     nombre_completo varchar2(80) not null,
-    num_cuenta varchar(9),
+    num_cuenta varchar2(9),
     expediente clob,
-    primary key(id),
-    constraint num_cuenta emplado_num_cuenta_uk unique(num_cuenta)
-)';
+    constraint pk_empleado primary key(id),
+    constraint empleado_num_cuenta_uk unique(num_cuenta)
+  )';
 end;
 /
+insert into empleado(id,nombre_completo,num_cuenta,expediente) values (1,'Vicente Romero Andrade','312097792',null);
+commit;
 -- C
-select * from user_segments where SEGMENT_NAME like '%EMPLEADO%';
+select 
+       SEGMENT_NAME, 
+       SEGMENT_TYPE, 
+       TABLESPACE_NAME,
+       BYTES,
+       BLOCKS,
+       EXTENTS 
+    from USER_SEGMENTS 
+        where SEGMENT_NAME like '%EMPLEADO%';
 -- D
-select * from user_segments us,user_LOBS ul where ul.SEGMENT_NAME=us.SEGMENT_NAME and table_name like '%EMPLEADO%';
+select
+       SEGMENT_NAME,
+       SEGMENT_TYPE,
+       TABLESPACE_NAME,
+       BYTES,
+       BLOCKS,
+       EXTENTS
+    from USER_SEGMENTS
+        where SEGMENT_NAME like '%EMPLEADO%'
+          or SEGMENT_NAME in (select SEGMENT_NAME from USER_LOBS ul where ul.TABLE_NAME = 'EMPLEADO')
+          or SEGMENT_NAME in (select INDEX_NAME from USER_LOBS ul where ul.TABLE_NAME = 'EMPLEADO');
 whenever sqlerror continue
